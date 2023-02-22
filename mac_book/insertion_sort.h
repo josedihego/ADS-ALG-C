9b297bad5f14e3d0d36bfc25601df657c64ec6c3
josedihego <josedihego@gmail.com>
2023-02-22 13:04:16 -0300

    with credits

--- mac_book/insertion_sort.h
+++ mac_book/insertion_sort.h
@@ -1,3 +1,6 @@
+//ALL CREDIT TO: Mastering Algorithms with C - Kyle Loudon 1999
+//From where this code or parts of it comes from
+
 int issort(void *data, 
            int size,
            int esize, 
