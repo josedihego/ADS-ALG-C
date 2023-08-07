f3dfd0e9376a5c5bef4ddba44ee81f1ab7565c5e
josedihego <josedihego@gmail.com>
2023-08-07 18:28:12 -0300

    before 2023.2

--- basic_data_structures/tabela_hash_produto.c
+++ basic_data_structures/tabela_hash_produto.c
@@ -19,27 +19,23 @@ void init_hash(HashTable * tabela, int tamanho){
     tabela->tamanho = tamanho;
 }
 
-
 int gerar_chave(Produto * produto){
     int chave = 0;
     int i = 0;
     while(produto->codigo[i]){
         chave = chave + produto->codigo[i];
-        i = i+1;
+        i = i + 1;
     }
     return chave;
 }
 
 int hash(Produto * produto, int tamanho){
     int chave = gerar_chave(produto);
-    float valor = sqrt(chave * 17)+(sqrt(5)-1);
+    float valor = sqrt(chave * 103)+(sqrt(5)-1);
     return (int)(valor)%tamanho;
 }
 
 int main(){
-    Produto * p = malloc(sizeof(Produto));
-    p->codigo = "789";
-    int a;
-    printf("%d\n",hash(p ,50));
+    printf("%s\n", "Fim");
     return EXIT_SUCCESS;
 }
