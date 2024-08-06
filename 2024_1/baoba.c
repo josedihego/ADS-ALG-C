fd3a8fb7d0af0d44cc42445b1043fcdd56c4c861
josedihego@gmail.com <josedihego@gmail.com>
2024-08-05 21:35:41 -0300

    with baoba.c

--- 2024_1/baoba.c
+++ 2024_1/baoba.c
@@ -28,3 +28,59 @@ MyTree * create_tree(Node * root){
     mt->root =  root;
     return mt;
 }
+
+MyTree * create_empty_tree(){
+    MyTree * mt =  malloc(sizeof(MyTree));
+    mt->root = NULL;
+    return mt;
+}
+
+void insert_tree(MyTree * t, Node * x){
+    if(t->root == NULL){
+        t->root = x;
+    }
+    else{
+        Node * y = t->root;
+        Node * smart;
+        while(y!=NULL){
+            smart = y;
+            if(x->key > y->key){
+                y = y -> right;
+            }
+            else{
+                y= y->left;
+            }
+        }
+        if(x->key > smart->key){
+            smart->right = x;
+        }
+        else{
+            smart->left = x;
+        }
+        x->p = smart;
+
+    }
+}
+int main(){
+    /**
+    MyTree * ar_pass = create_tree(create_node(56));
+    Node * no_45 = create_node(45);
+    ar_pass->root->left = no_45;
+    no_45->p = ar_pass->root;
+    Node * no_70 = create_node(70);
+    ar_pass->root->right = no_70;
+    no_70->p =  ar_pass->root;
+    printf("Fim");
+    * 
+     */
+    MyTree * ar_pass = create_empty_tree();
+    insert_tree(ar_pass, create_node(30));
+    insert_tree(ar_pass, create_node(79));
+    insert_tree(ar_pass, create_node(56));
+    insert_tree(ar_pass, create_node(21));
+    insert_tree(ar_pass, create_node(30));
+    printf("Fim");
+
+}
+
+
