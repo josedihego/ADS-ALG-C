d545ba565e100341b7bc953f666956466a5d5527
josedihego@gmail.com <josedihego@gmail.com>
2024-10-14 21:49:54 -0300

    go

--- 2024_2/insertion_sort.c
+++ 2024_2/insertion_sort.c
@@ -2,6 +2,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(){
-    
+void insertion_sort(int *A, int length)
+{
+    for (int j = 1; j < length; j = j + 1)
+    {
+        int key = A[j];
+        int i = j - 1;
+        while (i > -1 && A[i] > key)
+        {
+            A[i + 1] = A[i];
+            i = i - 1;
+        }
+        A[i + 1] = key;
+    }
+}
+
+void print_array(int *A, int length)
+{
+    for (int i = 0; i < length; i = i + 1)
+    {
+        if (i == length - 1)
+            printf("%d ", A[i]);
+        else
+            printf("%d, ", A[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+
+    int values[] = {27, 93, 60, 493, -1, 0, 10, 14, -20, 7};
+    int length = 10;
+    printf("\nAntes\n");
+    print_array(values, length);
+    insertion_sort(values, length);
+    printf("\nDepois\n");
+    print_array(values, length);
 }
