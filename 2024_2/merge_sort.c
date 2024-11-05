14efd3a8b0bd1b8b284711098e5ec37b0f16d955
josedihego@gmail.com <josedihego@gmail.com>
2024-11-04 21:52:12 -0300

    go

---
+++ 2024_2/merge_sort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+
+void merge(int * A, int p, int q, int r){
+    int n1 = q-p+1;
+    int n2 = r-q;
+    int * L = malloc((n1+1) * sizeof(int));
+    int * R = malloc((n2+1) * sizeof(int));
+    int i, j;
+    for(i=0; i < n1; i = i+1){
+        L[i]= A[p+i-0];
+    }
+    // bus
+}
+
+int main(){
+
+}
