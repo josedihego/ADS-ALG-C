169bb648b053776adf3ab5a58b36fd64d7de6eb9
josedihego@gmail.com <josedihego@gmail.com>
2025-01-27 21:44:09 -0300

    go

--- 2024_2/baoba.c
+++ 2024_2/baoba.c
@@ -26,6 +26,7 @@ Node * create_node(int key){
 
 My_Tree * create_empty_tree(){
   My_Tree * new_tree = malloc(sizeof(My_Tree));
+  new_tree->root = NULL;
   return new_tree;
 }
 
@@ -67,6 +68,46 @@ void inorder_tree_walk(Node * node){
     }
 }
 
+Node * tree_search(int key, My_Tree * t){
+    Node * x = t->root;
+    while(x!=NULL && x->key != key){
+        if(key < x->key){
+            x = x->left;
+        }
+        else{
+            x= x->right;
+        }
+    }
+    return x;
+}
+Node * tree_search_rec(int key, Node * x){
+    if(x==NULL || x->key == key) return x;
+    else{
+        if(key < x->key) return tree_search_rec(key, x->left);
+        else return tree_search_rec(key, x->right);
+    }
+}
+
+Node * tree_minimum(Node * node){
+    Node * x = node;
+    while(x->left != NULL){
+        x = x->left;
+    }
+    return x;
+}
+
+Node * tree_successor(Node * x){
+    if(x->right != NULL){
+        return tree_minimum(x->right);
+    }
+    Node * y = x;
+    while(y != NULL && y->right != x){
+        x = y;
+        y = y->mother;
+    }
+    return y;
+}
+
 int main(){
     My_Tree * t = create_empty_tree();
     insert_tree(create_node(5),t);
@@ -75,4 +116,31 @@ int main(){
     insert_tree(create_node(3),t);
     insert_tree(create_node(4),t);
     inorder_tree_walk(t->root);
+    printf("Resultados busca\n");
+
+    int key_for_search = 3;
+    Node * b1 = tree_search(key_for_search, t);
+    if(b1!=NULL) printf("Achei: %d\n", b1->key);
+    else printf("Não achei a chave %d\n",key_for_search);
+
+    key_for_search = 100;
+    Node * b2 = tree_search(key_for_search, t);
+    if(b2!=NULL) printf("Achei: %d\n", b2->key);
+    else printf("Não achei a chave: %d\n",key_for_search);
+
+
+    key_for_search = 3;
+    b1 = tree_search_rec(key_for_search, t->root);
+    if(b1!=NULL) printf("Achei com rec: %d\n", b1->key);
+    else printf("Não achei a chave com rec %d\n",key_for_search);
+
+    key_for_search = 100;
+    b2 = tree_search_rec(key_for_search, t->root);
+    if(b2!=NULL) printf("Achei com rec: %d\n", b2->key);
+    else printf("Não achei a chave com rec: %d\n",key_for_search);
+
+    Node * node_3 = tree_search(3, t);
+    Node * succ = tree_successor(node_3);
+    printf("Sucessor do no %d é %d\n", node_3->key, succ->key);
+
 }
