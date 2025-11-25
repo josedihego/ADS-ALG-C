dbe174f5e7b4ced93c502ea119b69809bcb0bdcf
josedihego@gmail.com <josedihego@gmail.com>
2025-11-24 21:43:54 -0300

    go

--- 2025_2/CRUD.c
+++ 2025_2/CRUD.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef struct Disciplina{
     char * nome;
@@ -40,6 +41,62 @@ void inserir_aluno_escola(Aluno * novo, Escola * escola){
     }
 }
 
+Aluno * buscar_aluno_escola(Aluno * aluno, Escola * escola){
+    bool achei = false;
+    int p = 0;
+    while(!achei && p < escola->prox){
+        if(strcmp(aluno->matricula,escola->alunos[p]->matricula)==0){
+            achei = true;
+        }
+    }
+    if(achei) return escola->alunos[p];
+    else return NULL;
+
+}
+
+int buscar_posicao_aluno_escola(Aluno * aluno, Escola * escola){
+    bool achei = false;
+    int p = 0;
+    while(!achei && p < escola->prox){
+        if(strcmp(aluno->matricula,escola->alunos[p]->matricula)==0){
+            achei = true;
+        }
+    }
+    if(achei) return p;
+    else return -1;
+
+}
+
 bool remover_aluno_escola(Aluno * aluno, Escola * escola){
-    
+    int posicao = buscar_posicao_aluno_escola(aluno,escola);
+    if(posicao != -1){
+        free(escola->alunos[posicao]);
+        escola->alunos[posicao] = escola->alunos[escola->prox-1];
+        escola->prox = escola->prox-1;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool atualizar_aluno_escola(Aluno * antigo, Aluno * novo, Escola * escola){
+    if(remover_aluno_escola(antigo,escola)){
+        inserir_aluno_escola(novo,escola);
+    }
+}
+
+
+int main(){
+    Escola * escola = malloc(sizeof(Escola));
+    escola->qnt_alunos = 1000;
+    escola->alunos = 
+        malloc(escola->qnt_alunos * sizeof(Aluno *));
+    escola->prox = 0;
+    Aluno * julio = malloc(sizeof(Aluno));
+    julio->matricula = "202.5345";
+    julio->idade = 19;
+    julio->nome = "Júlio";
+    inserir_aluno_escola(julio,escola);
+
 }
