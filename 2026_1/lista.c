32c062c5bbf9204cb026a2df81d4842023eb8302
josedihego@gmail.com <josedihego@gmail.com>
2026-04-27 21:04:01 -0300

    go

--- 2026_1/lista.c
+++ 2026_1/lista.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Profile{
+    char * public_link;
+    char * name;
+    struct ListIFBA * contacts;
+}Profile;
+
 typedef struct Node{
     int key;
+    Profile * profile;
     struct Node * next;
     struct Node * prev;
 }Node;
@@ -15,6 +22,13 @@ ListIFBA * create_list(){
     ListIFBA * n = malloc(sizeof(ListIFBA));
     n->head = NULL;
     return n;
+}
+
+Node * create_node(int key){
+    Node * n = malloc(sizeof(Node));
+    n->key = key;
+    n->next = NULL;
+    n->prev = NULL;
 }
 
 void insert(ListIFBA * l, Node * n){
@@ -36,7 +50,7 @@ Node * search(int key, ListIFBA * l){
     return x;
 }
 
-void remove(int key, ListIFBA * l){
+void remove_list(int key, ListIFBA * l){
     Node * r = search(key, l);
     if(r!=NULL){
         if(r->prev==NULL){
@@ -58,8 +72,21 @@ void remove(int key, ListIFBA * l){
     }
 }
 
-
+void print_list(ListIFBA * l){
+    printf("\n");
+    Node * x = l->head;
+    while(x!= NULL){
+        printf("<- %d ->", x->key);
+        x = x->next;
+    }
+    printf("\n");
+}
 
 int main(){
-
+    ListIFBA * nova_lista = create_list();
+    insert(nova_lista, create_node(8));
+    insert(nova_lista, create_node(6));
+    insert(nova_lista, create_node(5));
+    remove_list(5,nova_lista);
+    print_list(nova_lista);
 }
